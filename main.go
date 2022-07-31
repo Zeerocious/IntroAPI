83a9c04486c81ecb05c6f4d30267fe4fbef8e1aa
Zeerocious <zoro_z_99@hotmail.com>
2022-07-31 12:39:53 -0700

    Completed project (unsure if there are any more bugs)
    Collects user's followings and checks their description and URL for any NSFW Content.
    
    Using Go Routines to speed up the GetActualURL function and utilizing waitgroups and mutex to ensure data race doesnt happen
    
    Displays the user's nsfw followings and exposes them l0l

--- main.go
+++ main.go
@@ -12,13 +12,17 @@ import (
 	"reflect"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	twitter "github.com/g8rswimmer/go-twitter/v2"
 )
 
 var config *Config
+var nsfw = regexp.MustCompile(`((?i)(NSFW|ONLYFANS|MODEL))|18\+|🔞`)
+var nsfwLink = regexp.MustCompile(`((?i)(ONLYFANS|FANSLY|FANHOUSE|CASHAPP))`)
 
+// 18+, NSWF, OF(case sensitive), onlyfans, 18+ emoji
 const (
 	TWITTER_API_BASE = "https://api.twitter.com"
 )
@@ -71,17 +75,60 @@ func main() {
 		person = UserSearch(client)
 
 	}
-	fmt.Println(person[0])
 	var ctx context.Context
 	var nextToken string
 	following, err := FollowingLookup(ctx, person[0].ID, client, nextToken)
-	if err != nil {
-		log.Panicf("user following lookup error: %v", err)
+	exposed := Expose(following)
+	fmt.Printf("%s is following %d NSFW twitter accounts.\n The accounts are: ", person[0].UserName, len(exposed))
+
+	for i := 0; i < len(exposed); i++ {
+		if i == len(exposed)-1 {
+			fmt.Println(exposed[i].UserName)
+		} else {
+			fmt.Printf(exposed[i].UserName + ", ")
+		}
 	}
-	println(len(following))
-	//for i := 0; i < len(following); i++ {
-	//fmt.Printf("Username: " + following[i].UserName + "\t Bio:\t" + following[i].Description + "\n\n")
-	//}
+}
+func getActualUrl(url string) (string, error) {
+	resp, err := http.Head(url)
+	if err != nil {
+		return "", err
+	}
+
+	finalURL := resp.Request.URL.String()
+
+	return finalURL, nil
+}
+
+func Expose(users []*twitter.UserObj) []*twitter.UserObj {
+	exposedUsers := []*twitter.UserObj{}
+	mtx := new(sync.Mutex)
+	var wg sync.WaitGroup
+	for i := 0; i < len(users); i++ {
+		wg.Add(1)
+		go func(user *twitter.UserObj) {
+			if nsfw.MatchString(user.Description) {
+				mtx.Lock()
+				exposedUsers = append(exposedUsers, user)
+				mtx.Unlock()
+				wg.Done()
+				return
+			}
+			url, err := getActualUrl(user.URL)
+			if err != nil {
+				wg.Done()
+				return
+			}
+			if nsfwLink.MatchString(url) {
+				mtx.Lock()
+				exposedUsers = append(exposedUsers, user)
+				mtx.Unlock()
+			}
+			wg.Done()
+		}(users[i])
+	}
+	wg.Wait()
+	return exposedUsers
 }
 func UsernameLookup(ctx context.Context, username string, client *twitter.Client) ([]*twitter.UserObj, error) {
 
@@ -96,14 +143,13 @@ func UsernameLookup(ctx context.Context, username string, client *twitter.Client
 	return userResponse.Raw.Users, err
 }
 func FollowingLookup(ctx context.Context, id string, client *twitter.Client, nextToken string) ([]*twitter.UserObj, error) {
-
 	opts := twitter.UserFollowingLookupOpts{
-		UserFields:      []twitter.UserField{twitter.UserFieldDescription},
+		UserFields:      []twitter.UserField{twitter.UserFieldDescription, twitter.UserFieldURL},
 		PaginationToken: nextToken,
+		MaxResults:      1000,
 	}
 
 	userResponse, err := client.UserFollowingLookup(context.Background(), id, opts)
-
 	if rateLimit, has := twitter.RateLimitFromError(err); has && rateLimit.Remaining == 0 {
 		time.Sleep(time.Until(rateLimit.Reset.Time()))
 		userResponse, err = client.UserFollowingLookup(context.Background(), id, opts)
@@ -120,7 +166,6 @@ func FollowingLookup(ctx context.Context, id string, client *twitter.Client, nex
 	return userResponse.Raw.Users, err
 }
 
-// 18+, NSWF, OF(case sensitive), Onlyfans, 18+ emoji
 func (a authorize) Add(req *http.Request) {
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
 }
@@ -142,13 +187,10 @@ func input(x []string, err error) []string {
 func UserSearch(c *twitter.Client) []*twitter.UserObj {
 	fmt.Printf("Enter the username of the person you're searching (must be 1 word): ")
 	people := input([]string{}, nil)
-	fmt.Println(people)
-	fmt.Println(people)
-	for len(people) != 1 || !regexp.MustCompile(`^[a-zA-Z]*$`).MatchString(people[0]) {
+	for len(people) != 1 || !regexp.MustCompile(`^[a-zA-Z0-9_]*$`).MatchString(people[0]) {
 		fmt.Printf("username entered incorrectly, try again: ")
 		people = input([]string{}, nil)
 	}
-	fmt.Println(people)
 	fmt.Println("Searching up " + people[0])
 	var ctx context.Context
 	resp, err := UsernameLookup(ctx, people[0], c)
